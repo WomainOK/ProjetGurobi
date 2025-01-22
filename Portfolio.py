2fd00959d8aef297a170ef725cd422a76afd0dd7
WomainOK <439933@etud.uco.fr>
2025-01-22 10:43:32 +0100

    Portfolio.py + Requirements.txt

--- Portfolio.py
+++ Portfolio.py
@@ -7,16 +7,33 @@ from gurobipy import GRB
 with open("data/portfolio-example.json", "r") as f:
     data = json.load(f)
 
-n = data["num_assets"]
-sigma = np.array(data["covariance"])
-mu = np.array(data["expected_return"])
-mu_0 = data["target_return"]
-k = data["portfolio_max_size"]
+n = data["num_assets"] # Nombre d'actif
+sigma = np.array(data["covariance"]) # Risque
+mu = np.array(data["expected_return"]) # Retour attendu
+mu_0 = data["target_return"] # Objectif retour
+k = data["portfolio_max_size"] # Taille maximal du portfolio
 
 
 with gp.Model("portfolio") as model:
     # Name the modeling objects to retrieve them
-    # ...
+        # Variable x : Investissements relatfis en actifs
+    x = model.addVars(n,vtype=GRB.CONTINUOUS, name="x")
+        # Variable binaire conrtôlant si l'actif i est négocié
+    y = model.addVars(n, vtype=GRB.BINARY, name="y")
+
+    # Contraintes
+        # Rendement minimal : Le rendement attendu du portefeuille doit dépasser mu_0
+    model.addConstr(sum(x[i] * mu[i] for i in range(n)) >= mu_0, name="return")
+        # Limite d'investissement : Le portefeuille peut inclure au maximum k actif
+    model.addConstr(y.sum() <= k,name="risk")
+        # Lien entre xi et yi : Si un actif i n'est pas sélectionné (yi = 0), alors xi = 0
+    for i in range(n):
+        model.addConstr(x[i] <= y[i], name=f"asset_{i}")
+        # Allocation totale : La somme des fractions investies doit être égale à 1
+    model.addConstr(x.sum() == 1, name="allocation")
+
+    model.setObjective(sum(x[i] * x[j] * sigma[i,j] for j in range(n)
+                                              for i in range(n)), sense=GRB.MINIMIZE)
 
     model.optimize()
 
